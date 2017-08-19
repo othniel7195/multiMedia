8ad9e6448a8e3b253040bee5c96fdbc9075e9a16
wszf <478043385@qq.com>
2017-08-19 17:58:14 +0800

    创建项目

---
+++ multiMedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  multiMedia
+//
+//  Created by 赵锋 on 2017/8/19.
+//  Copyright © 2017年 赵锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ multiMedia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  multiMedia
+//
+//  Created by 赵锋 on 2017/8/19.
+//  Copyright © 2017年 赵锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
